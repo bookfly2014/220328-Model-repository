f1985d5566424b0118de3c8f16ad053638852fa2
Maximo <maximogabriel270608@gmail.com>
2022-03-29 00:03:24 -0500

    palawan2: primera version

--- lolo.js
+++ lolo.js
@@ -3,9 +3,9 @@ let x = 5;         // assign the value 5 to x
 let y = 2;         // assign the value 2 to y
 let z = x + y;     // assign the value 7 to z (5 + 2)
 //Multiplyinig
-let x = 5;
-let y = 2;
-let z = x * y;
+let x = 50;
+let y = 20;
+let z = x * y;     // assign the value 1000 to z (50 * 20)
 // reamind
 let x = 5;
 let y = 2;
@@ -19,5 +19,5 @@ let x = 5;
 x--;
 let z = x;
 //power
-let x = 5;
-let z = x ** 2;          // result is 25
+let x = 50;
+let z = x ** 2;          // result is 2500
