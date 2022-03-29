c01665a4edb0034aaeb600494028bf13be800c56
Neo <bookfly2014@gmail.com>
2022-03-28 23:23:28 -0500

    master: primer comentario

---
+++ lolo.js
@@ -0,0 +1,16 @@
+// adding
+let x = 5;         // assign the value 5 to x
+let y = 2;         // assign the value 2 to y
+let z = x + y;     // assign the value 7 to z (5 + 2)
+//Multiplyinig
+let x = 5;
+let y = 2;
+let z = x * y;
+//Divide
+let x = 5;
+let y = 2;
+let z = x / y;
+//power
+let x = 5;
+let y = 2;
+let z = x * y;
