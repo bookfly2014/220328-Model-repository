69352b17e974b50866173201ff4c6cd16a698cbe
Maximo <maximogabriel270608@gmail.com>
2022-03-28 23:51:22 -0500

    palawan1: primer avance

--- lolo.js
+++ lolo.js
@@ -6,3 +6,18 @@ let z = x + y;     // assign the value 7 to z (5 + 2)
 let x = 5;
 let y = 2;
 let z = x * y;
+// reamind
+let x = 5;
+let y = 2;
+let z = x % y;
+// Incremmenting
+let x = 5;
+x++;
+let z = x;
+//Decrementing
+let x = 5;
+x--;
+let z = x;
+//power
+let x = 5;
+let z = x ** 2;          // result is 25
